318ba5364033b4b01d75561de43d62954903dc86
Kris Thom White <kris.white@eightounce.com>
2020-05-04 18:27:39 -0500

    feat: updating app with shop page, collection preview and coll item

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-
-const HatsPage = () => (
-  <div>
-    <h2>Hats page</h2>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage}/>
-        <Route path='/shop/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
